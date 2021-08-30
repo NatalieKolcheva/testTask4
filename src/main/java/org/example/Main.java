1095b215a84ccfbce0eac69afb8442e6ee79fbe7
NatalieKolcheva <natalykolcheva@gmail.com>
2021-08-30 13:10:00 +0300

    sum of numbers from the file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
 
+    public static void main(String[] args) throws Exception {
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("numbers.txt");
+        Scanner scan = new Scanner(inputStream);
+        Double number;
+        Double sum = 0.0;
+
+        while (scan.hasNextLine()) {
+            try {
+                number = Double.parseDouble(scan.nextLine());
+                sum = sum + number;
+
+            } catch (NumberFormatException ne) {
+
+            }
+
+            inputStream.close();
+        }
+        System.out.println("Sum of numbers in the file: " + sum);
+
+
+    }
 }
